b631b279ee1bdd21169f86f897d3eb8e02b03779
faithque <52964212+faithque@users.noreply.github.com>
2024-09-23 13:38:30 +1000

    Add files via upload

--- startServerWithMongo.js
+++ startServerWithMongo.js
@@ -1,14 +1,14 @@
 let express = require('express');
 let app = express();
-const bcrypt = require('bcrypt');
+const argon2 = require('argon2');
 const User = require('./models/user');
-const { MongoClient, ServerApiVersion } = require('mongodb') // Optional if using express-async-handler
+const UserLinks = require('./models/userLinks');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb') // Optional if using express-async-handler
 
 // const uri = "mongodb://localhost:27017";
 const uri = "mongodb+srv://wandabwafaith:Mukongolo2472@cluster0.anov8lm.mongodb.net/"
 // const uri = "mongodb+srv://s223749059:2fICYltL4sfxoG1M@cluster0.tjcoayf.mongodb.net/"
 let port = process.env.port || 3000;
-let collection;
 
 app.use(express.static(__dirname + '/public'))
 app.use(express.json());
@@ -26,7 +26,6 @@ const client = new MongoClient(uri, {
 async function runDBConnection() {
     try {
         await client.connect();
-        collection = client.db().collection('Cat');
         console.log("DB connected");
     } catch(ex) {
         console.error(ex);
@@ -36,31 +35,6 @@ async function runDBConnection() {
 app.get('/', function (req,res) {
     res.render('indexMongo.html');
 });
-
-app.get('/api/cats', (req,res) => {
-    getAllCats((err,result)=>{
-        if (!err) {
-            res.json({statusCode:200, data:result, message:'get all cats successful'});
-        }
-    });
-});
-
-app.post('/api/cat', (req,res)=>{
-    let cat = req.body;
-    postCat(cat, (err, result) => {
-        if (!err) {
-            res.json({statusCode:201, data:result, message:'success'});
-        }
-    });
-});
-
-function postCat(cat,callback) {
-    collection.insertOne(cat,callback);
-}
-
-function getAllCats(callback){
-    collection.find({}).toArray(callback);
-}
 
 app.get('/login', (req, res) => {
     res.render('login.html');
@@ -72,13 +46,76 @@ app.post('/login', async (req, res) => {
     if (!user) {
         return res.status(401).send('Invalid username or password');
     }
-    const isPasswordValid = await bcrypt.compare(password, user.password);
+    const isPasswordValid = await argon2.verify(user.password, password);
     if (!isPasswordValid) {
         return res.status(401).send('Invalid username or password');
     }
     // Create and send an authentication token
     const token = createAuthToken(user);
     res.status(200).send({ token });
+});
+
+app.get('/users', async (req, res) => {
+    try{
+        const users = await User.find({}).sort({username:1}).toArray();
+        if ((await User.countDocuments({})) === 0) {
+            return res.status(401).send("No users found.");
+        }
+        res.status(200).send({users});
+    }catch(err){
+        return res.status(400).json({ error: err.message });
+    }   
+});
+
+app.post('/user', async (req, res) => {
+    const { username } = req.body;
+    try{
+        const user = await User.findOne({username});
+        if (!user) {
+            return res.status(401).send('Invalid username');
+        }
+        res.status(200).send({user});
+    }catch(err){
+        return res.status(400).json({ error: err.message });
+    }   
+});
+
+app.post('/userlinks', async (req, res) => {
+    const { username } = req.body;
+    try{
+        // Check if username is not valid
+        const existingUser = await User.findOne({ username });
+        if (!existingUser) {
+            throw new Error('Username does not exist');
+        }
+        // fetch the user's social media links
+        const userLinks = await UserLinks.find({username}).sort({platform: 1}).toArray();
+        res.status(200).send({userLinks});
+    }catch(err){
+        return res.status(400).json({ error: err.message });
+    }   
+});
+
+app.post('/userlink', async (req, res) => {
+    const { id  } = req.body;
+    // Validate ObjectId format
+    if (!ObjectId.isValid(id)) {
+        console.error("Invalid ObjectId format:", id);
+        return res.status(400).json({ error: 'Invalid ID format.' });
+    }
+    const objectId = new ObjectId(id);
+    console.log("Converted ObjectId:",objectId);
+    try{
+        // Check if username is not valid
+        const userLink = await UserLinks.findOne({ _id: objectId });
+        if (!userLink) {
+            throw res.status(404).json({ error: 'Link does not exist.' });
+        }
+        // fetch the user's social media links
+        res.status(200).send({userLink});
+    }catch(err){
+        return res.status(400).json({ error: err.message });
+    }   
 });
 
 app.get('/register', (req, res) => {
@@ -98,7 +135,7 @@ app.post('/register', async (req, res) => {
             throw new Error('Username already exists');
         }
         // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await argon2.hash(password);
         // Create a new user
         await User.insertOne({
             username,
@@ -106,6 +143,64 @@ app.post('/register', async (req, res) => {
             password: hashedPassword
         });
         res.status(200).send({'message':'Registration successful'});
+    }catch(err){
+        return res.status(400).json({ error: err.message }); // Use JSON for structured response
+        
+    }
+});
+
+app.post('/addSocialMedia', async (req, res) => {
+    const { username, platform, link } = req.body;
+    try{
+        // Check for missing fields
+        if (!username || !link) {
+            throw new Error('Username and link are required.');
+        }
+    
+        // Check if username is not valid
+        const existingUser = await User.findOne({ username });
+        if (!existingUser) {
+            throw new Error('Username does not exist');
+        }
+        // Create a new user social media link
+        await UserLinks.insertOne({
+            username,
+            platform,
+            link
+        });
+        res.status(200).send({'message':'Added social media link'});
+    }catch(err){
+        return res.status(400).json({ error: err.message }); // Use JSON for structured response
+        
+    }
+});
+
+app.post('/updateSocialMedia', async (req, res) => {
+    const { username, platform, link } = req.body;
+    try{
+        // Check for missing fields
+        if (!username || !link) {
+            throw new Error('Username and link are required.');
+        }
+    
+        // Check if username is not valid
+        const existingUser = await User.findOne({ username });
+        if (!existingUser) {
+            throw new Error('Username does not exist');
+        }
+        // Create a new user social media link
+        await UserLinks.updateOne(
+            {
+            username
+            },
+            {
+                $set: {platform, link}                
+            },
+            { 
+                upsert: true 
+            }
+        );
+        res.status(200).send({'message':'Updated social media link'});
     }catch(err){
         return res.status(400).json({ error: err.message }); // Use JSON for structured response
         
